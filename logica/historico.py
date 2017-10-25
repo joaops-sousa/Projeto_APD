a7c891e6069a5100dedc235c2e8c0cbe7192fa7c
LucasKeiti <33075444+LucasKeiti@users.noreply.github.com>
2017-10-24 22:41:40 -0200

    Modificado o modulo historico

--- logica/historico.py
+++ logica/historico.py
@@ -4,7 +4,11 @@ import filme
 filmes_assistidos=[]
 
 def registrar_filme_assistido(cod_filme,cpf):
-    pass
+    filme_assistido=[cpf,cod_filme]
+    filmes_assistidos.append(filme_assistido)
 
 def listar_filmes_assistidos(cpf):
-    pass
+    usuario=usuario.buscar_usuario(cpf)
+    cpf= usuario[0]
+    nome=usuario[1]
+    
