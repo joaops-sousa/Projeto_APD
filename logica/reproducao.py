70bf74c48541c264dcd17434ac04780070a1c632
João Pedro da Silva Sousa <jps.soousa@gmail.com>
2017-10-31 22:40:01 -0200

    Add files via upload

--- logica/reproducao.py
+++ logica/reproducao.py
@@ -2,7 +2,6 @@ from logica import usuario
 from logica import filme
 from logica import historico
 
-avaliacoes = []
 
 def reproduzir_filme(cpf,cod_filme):
     user = usuario.buscar_usuario(cpf)
@@ -12,27 +11,3 @@ def reproduzir_filme(cpf,cod_filme):
     else:
         historico.registrar_filme_assistido(cod_filme,cpf)
         return True
-
-def avaliar_filme(cpf,cod_filme,nota):
-    user = usuario.buscar_usuario(cpf)
-    movie = filme.buscar_filme(cod_filme)
-    if user == None or movie == None:
-        return False
-    else:
-        avaliacao = [cpf,user[1],cod_filme,movie[1],nota]
-        avaliacoes.append(avaliacao)
-        return True
-
-def listar_avaliacoes():
-    return avaliacoes
-
-def listar_avaliacoes_por_cpf(cpf):
-    aux = []
-    for a in avaliacoes:
-        if cpf == a[0]:
-            aux.append(a)
-    return aux
-
-def remover_todas_avaliacoes():
-    global avaliacoes
-    avaliacoes = []
